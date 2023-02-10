8be6dad39c07df98432a434d380cbc205fca2bb3
Daniel Simoni Wegher <w.daniels@hotmail.com>
2023-02-09 21:32:04 -0300

    'cleaning'

--- function.js
+++ function.js
@@ -9,25 +9,3 @@ setTimeout(DezSegundos, 1000*10);
 
 
 
-function timer() {
-    if ((millisecond += 10) == 1000) {
-      millisecond = 0;
-      second++;
-    }
-    if (second == 60) {
-      second = 0;
-      minute++;
-    }
-    if (minute == 60) {
-      minute = 0;
-      hour++;
-    }
-    document.getElementById('hour').innerText = returnData(hour);
-    document.getElementById('minute').innerText = returnData(minute);
-    document.getElementById('second').innerText = returnData(second);
-    document.getElementById('millisecond').innerText = returnData(millisecond);
-  }
-  
-  function returnData(input) {
-    return input >= 10 ? input : `0${input}`
-  }
