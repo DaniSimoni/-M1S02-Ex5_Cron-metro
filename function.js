ff2263f1597ed6dca67896342c5b243d8ac5a583
Daniel Simoni Wegher <w.daniels@hotmail.com>
2023-02-13 16:21:20 -0300

    refatorando

--- function.js
+++ function.js
@@ -1,11 +1,67 @@
 /* Crie um cronômetro que seja executado por um botão, conte 10 segundos, e no fim imprima um texto: 'Seu tempo acabou!! Tente novamente!!'. */
 
+const button = document.getElementById('myBtn');
+const form = document.getElementById('form');
+
+ function DezSegundos() {
+
+        alert("Seu tempo acabou!! Tente novamente!!");
+    }
+ 
 
 
-function DezSegundos(){
+button.addEventListener('click', function(event) {
+    event.preventDefault();
+    setTimeout(DezSegundos, 1000*3);
+
+});
+
+
+
+
+   
+   
+ 
+
+  
+
+
+
+
+/*   function DezSegundos() {
+    e.preventDefault();
+    alert("Seu tempo acabou!! Tente novamente!!");
+}
+setTimeout(DezSegundos, 1000*10); */
+
+
+
+
+
+/* 
+function clicou () {
+    document.getElementById("myBtn").addEventListener("click", displayDate);
+    element.addEventListener("mrBtn", function(){ alert("Hello World!"); });
+
+    const time = 0;
+
+    if (clicou === true) {
+        DezSegundos();
+} else {
+        refresh();
+
+}};
+
+
+
+function DezSegundos() {
+    e.preventDefault();
+
+
     alert("Seu tempo acabou!! Tente novamente!!");
 }
 setTimeout(DezSegundos, 1000*10);
 
 
 
+ */
